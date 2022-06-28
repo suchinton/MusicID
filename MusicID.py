8c299bd7f54d66a2140de4becefe92734ebfb742
suchinton <75079303+suchinton@users.noreply.github.com>
2022-06-28 22:31:35 +0530

    Implemented threading
    Implemented Threading so that the Qt GUI stays responsive during Updating of Database and searching for new track

--- MusicID.py
+++ MusicID.py
@@ -1,11 +1,11 @@
-from cgitb import reset
-from operator import le
-from pickle import FALSE
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLineEdit, QPushButton,QToolButton, QFileDialog, QTextEdit
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLineEdit, QPushButton,QToolButton, QFileDialog, QTextEdit, QLabel
 from PyQt5 import uic
+from PyQt5.QtGui import QImage, QPixmap
 import sys
 import os
 import Recorder
+from threading import *
+import requests
 
 class ResultWindow(QMainWindow):
     def __init__(self):
@@ -15,7 +15,11 @@ class ResultWindow(QMainWindow):
         uic.loadUi("Result.ui",self)
 
         # Define Widgets
-        self.Display = self.findChild(QTextEdit,"Display")        
+        self.Display = self.findChild(QTextEdit,"Display")
+        self.Art = self.findChild(QLabel,"Art")
+        #self.Artist_L = self.findChild(QLabel,"Artist_L")        
+        #self.Track_L = self.findChild(QLabel,"Track_L")
+        #self.Album_L = self.findChild(QLabel,"Album_L")
 
 
 class MainWindow(QMainWindow):
@@ -33,11 +37,21 @@ class MainWindow(QMainWindow):
 
         # Actions to events
         self.toolbutton.clicked.connect(self.PickDir)
-        self.Submit.clicked.connect(self.Index_refresh)
-        self.Listen.clicked.connect(self.SearchDB)
+        self.Submit.clicked.connect(self.T_Index_refresh)
+        self.Listen.clicked.connect(self.T_SearchDB)
 
         # Show App
         self.show()
+    
+    ## Thread Fns to keep GUI responsive
+    def T_SearchDB(self):
+        self.t = Thread(target = self.SearchDB)
+        self.t.start()
+        self.t.join()
+
+    def T_Index_refresh(self):
+        t = Thread(target=self.Index_refresh)
+        t.start()
 
     def PickDir(self):
         folderpath = QFileDialog.getExistingDirectory(self, 'Select Folder')
@@ -49,28 +63,47 @@ class MainWindow(QMainWindow):
             for name in files:
                 os.system(f"cd lib && ./musicID index {os.path.join(root,name)} >> db")
         os.system("cd lib && cat db | grep /")
-                
 
     def SearchDB(self):
-        self.Listen.setText("Listening... ﳃ")
         self.Listen.setEnabled(False)
+        self.Listen.setText("Listening... ﳃ")
         Recorder.rec()
-        self.Listen.setText("Listen... ")
-        self.Listen.setEnabled(True)
+        self.Listen.setText("Searching... ")
         os.system("cd lib && ./musicID search ../output.wav db > ~/MusicID/Status.txt")
 
         text_file = open("Status.txt", "r")
         result = text_file.read()
 
         print(result)
-
+        
         self.show_result = ResultWindow()
+
         self.show_result.Display.setText(result)
+
+
+        #self.url_img = 'https://i.scdn.co/image/ab67616d0000b273c8a11e48c91a982d086afc69'
+
+        #self.image = QImage()
+        #self.image.loadFromData(requests.get(self.url_img).content)
+        #self.show_result.Art.setPixmap(QPixmap(self.image))
+         
+        #self.artist = os.system("cat Status.txt | grep artist")
+        #self.show_result.Artist_L.setText(self.artist)
+#
+        #self.track = os.system("cat Status.txt | grep Track")
+        #self.show_result.Track_L.setText(self.track)
+#
+        #self.album = os.system("cat Status.txt | grep Album")
+        #self.show_result.Album_L.setText(self.album)
+#
         self.show_result.show()
 
         text_file.close()
         os.remove("output.wav")
         os.remove("Status.txt")
+        
+        self.Listen.setText("Listen... ")
+        self.Listen.setEnabled(True)
 
 # init the app
 
